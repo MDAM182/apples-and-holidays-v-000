b246f642237da36029977fcaeab871c3a31fc0e9
Michael Morris <mdam2345@gmail.com>
2018-06-26 02:57:36 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -32,7 +32,7 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  holiday_supplies[:memorial_day]
+  holiday_supplies[:spring][:memorial_day]
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
 
